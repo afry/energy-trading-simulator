f75f5841d859d759d46f8a374c6f50b84da6e690
= <=>
2021-10-27 14:34:54 +0200

    Made agent interface with abstract method definitions and deined class signatures for concrete agent implementations

--- agent.py
+++ agent.py
@@ -1,9 +1,34 @@
-class Agent:
-    """Agent class"""
+from abc import ABC, abstractmethod
+
+
+class IAgent(ABC):
+    """Interface for agents to implement"""
     def __init__(self, type, production, consumption):
         self.type = type
         self.production = production
         self.consumpton = consumption
     
+    @abstractmethod
     def make_bid():
         # Make a bid for produced or needed energy for next time step
+        pass
+    
+
+    def make_prognosis():
+        # Make resource prognosis for the trading horizon
+        pass
+
+class BuildingAgent(IAgent):
+    #TODO: Implement
+
+
+class PVAgent(IAgent):
+    #TODO: Implement
+
+
+class GridSellerAgent(IAgent):
+    #TODO: Implement
+
+
+class GridBuyerAgent(IAgent):
+    #TODO: Implement
