e994ce3ba3abf84efbecbe59f78c55269da3d05c
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-11-01 15:31:47 +0100

    FEATURE - RES-103 - Started implementing GridAgent. The question of state data needed by agents is interesting. A storage would presumably need to keep track of how much energy it has in store, but other agents perhaps make sense to keep stateless? Also, to proceed we need a "Bid" class

--- agent.py
+++ agent.py
@@ -1,67 +1,73 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
 
 class IAgent(ABC):
     """Interface for agents to implement"""
-    def __init__(self, type, production, consumption):
-        # What kind of state data should an agent keep? Any at all?
-        self.type = type
-        self.production = production
-        self.consumpton = consumption
-    
+
+    # def __init__(self):
+
     @abstractmethod
-    def make_bid():
+    def make_bid(self, period):
         # Make a bid for produced or needed energy for next time step
         pass
-    
+
     @abstractmethod
-    def make_prognosis():
+    def make_prognosis(self, period):
         # Make resource prognosis for the trading horizon
         pass
 
+
 class BuildingAgent(IAgent):
-    #TODO: Implement
-    def make_bid():
+    # TODO: Implement
+    def make_bid(self, period):
         # The buidling should make a bid for purchasing energy
         pass
 
-
-    def make_prognosis():
+    def make_prognosis(self, period):
         # The building should make a prognosis for how much energy will be required
         pass
 
 
 class PVAgent(IAgent):
-    #TODO: Implement
-    def make_bid():
+    # TODO: Implement
+    def make_bid(self, period):
         # The PV park should make a bid to sell energy
         pass
 
-
-    def make_prognosis():
+    def make_prognosis(self, period):
         # The PV park should make a prognosis for how much energy will be produced
         pass
 
 
-class GridSellerAgent(IAgent):
-    #TODO: Implement
-    def make_bid():
-        # Always sell maximum/infinite amount at the grid price
+class GridAgent(IAgent):
+    nordpool_data: pd.DataFrame
+    MAX_TRANSFER_PER_HOUR = 4000  # kW (placeholder value: same limit as FED)
+
+    def __init__(self, external_price_csv='data/nordpool_area_grid_el_price.csv'):
+        self.nordpool_data = pd.read_csv(external_price_csv, index_col=0)
+        if self.nordpool_data.mean()[0] > 100:
+            # convert price from SEK per MWh to SEK per kWh
+            self.nordpool_data = self.nordpool_data / 1000
+        self.nordpool_data.columns = ['price_sek_kwh']
+
+    def make_bid(self, period):
+        # Submit 2 bids here
+        # Sell up to MAX_TRANSFER_PER_HOUR kWh at calculate_retail_price(period)
+        # Buy up to MAX_TRANSFER_PER_HOUR kWh at calculate_wholesale_price(period)
+        # TODO: Build bid
         pass
 
+    def calculate_retail_price(self, period):
+        """Returns the price at which the agent is willing to sell electricity, in SEK/kWh"""
+        # Per https://doc.afdrift.se/pages/viewpage.action?pageId=17072325
+        return self.nordpool_data.loc[period].iloc[0] + 0.48
 
-    def make_prognosis():
-        # Get the grid selling price
-        pass
+    def calculate_wholesale_price(self, period):
+        """Returns the price at which the agent is willing to buy electricity, in SEK/kWh"""
+        # Per https://doc.afdrift.se/pages/viewpage.action?pageId=17072325
+        return self.nordpool_data.loc[period].iloc[0] + 0.05
 
-
-class GridBuyerAgent(IAgent):
-    #TODO: Implement
-    def make_bid():
-        # Always buy maximum/infinite at wholesale price
-        pass
-
-
-    def make_prognosis():
-        # Get wholesale price
+    def make_prognosis(self, period):
+        # Not sure what this method should return
         pass
---
+++ tests/test_agent.py
@@ -0,0 +1,17 @@
+import unittest
+import agent
+
+
+class TestGridAgent(unittest.TestCase):
+
+    gridAgent = agent.GridAgent('../data/nordpool_area_grid_el_price.csv')
+
+    def test_retail_price(self):
+        self.assertEqual(self.gridAgent.calculate_retail_price("2019-01-01 00:00:00"), 0.58315)
+
+    def test_wholesale_price(self):
+        self.assertEqual(self.gridAgent.calculate_wholesale_price("2019-01-01 00:00:00"), 0.15315)
+
+
+if __name__ == '__main__':
+    unittest.main()
