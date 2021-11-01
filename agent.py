9ad76bf040cdcfaa2ac723c80c8c655737c25dcd
= <=>
2021-11-01 15:06:07 +0100

    Added a bid class for the trading contracts

--- agent.py
+++ agent.py
@@ -9,21 +9,26 @@ class IAgent(ABC):
         self.production = production
         self.consumpton = consumption
     
+
     @abstractmethod
     def make_bid():
         # Make a bid for produced or needed energy for next time step
         pass
     
+
     @abstractmethod
     def make_prognosis():
         # Make resource prognosis for the trading horizon
         pass
 
+
 class BuildingAgent(IAgent):
     #TODO: Implement
     def make_bid():
         # The buidling should make a bid for purchasing energy
-        pass
+        bids = []
+
+        return bids
 
 
     def make_prognosis():
@@ -35,7 +40,9 @@ class PVAgent(IAgent):
     #TODO: Implement
     def make_bid():
         # The PV park should make a bid to sell energy
-        pass
+        bids = []
+
+        return bids
 
 
     def make_prognosis():
@@ -43,11 +50,32 @@ class PVAgent(IAgent):
         pass
 
 
-class GridSellerAgent(IAgent):
+class BatteryStorageAgent(IAgent):
     #TODO: Implement
     def make_bid():
-        # Always sell maximum/infinite amount at the grid price
+        # The Battery storage should generally buy if capacity is low and sell if capacity is high
+        # Logic sketch:
+        # 1. Start empty
+        # 2. Buy until capacity exceeds some threshold
+        # 3. Sell until below some other threshold
+        # 4. GOTO 2
+        bids = []
+
+        return bids
+
+
+    def make_prognosis():
+        # Get the current capacity of the battery storage
         pass
+
+
+class GridAgent(IAgent):
+    #TODO: Implement
+    def make_bid():
+        # Always make one bid to buy at wholesale price, and one bid to sell att market price
+        bids = []
+
+        return bids
 
 
     def make_prognosis():
@@ -55,13 +83,17 @@ class GridSellerAgent(IAgent):
         pass
 
 
-class GridBuyerAgent(IAgent):
-    #TODO: Implement
-    def make_bid():
-        # Always buy maximum/infinite at wholesale price
-        pass
-
-
-    def make_prognosis():
-        # Get wholesale price
-        pass
+class Bid():
+    """The bid model for our trading platform.
+    
+    Parameters:
+        Action: Buy/sell
+        Resource: Electricity
+        Quantity: Amount in kWh
+        Price: SEK/kWh
+    """
+    def __init__(self, action, resource, quantity, price):
+        self.action = action
+        self.resource = resource
+        self.quantity = quantity
+        self.price = price
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
     #Perform the main loop
+
+    #Register all agents
+
+    #Import simulation span? List of timestamps, etc
+
+    #Main loop
+    timesteps = []
+    for step in timesteps:
+        # Get all bids
+        # Resolve bids
+        pass
+    
+    # Exit gracefully
     pass
 
 
