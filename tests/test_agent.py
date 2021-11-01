aae0fdb5268c21e10a2281b4e73e7343ccbb6a5a
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-11-01 16:03:02 +0100

    TEST - fixed test following my previous commit

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -4,7 +4,7 @@ import agent
 
 class TestGridAgent(unittest.TestCase):
 
-    gridAgent = agent.GridAgent('../data/nordpool_area_grid_el_price.csv')
+    gridAgent = agent.ElectricityGridAgent('../data/nordpool_area_grid_el_price.csv')
 
     def test_retail_price(self):
         self.assertEqual(self.gridAgent.calculate_retail_price("2019-01-01 00:00:00"), 0.58315)
