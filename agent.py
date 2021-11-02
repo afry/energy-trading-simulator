196d0f287ace93f59d213246d6e0fda61527a480
= <=>
2021-11-02 11:22:13 +0100

    Battery agent revisions

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import math
 
 
 class IAgent(ABC):
@@ -47,25 +48,32 @@ class PVAgent(IAgent):
 
 
 class BatteryStorageAgent(IAgent):
+    """The agent for a battery storage actor.
+    
+    The battery works on the logic that it tries to keep it capacity between an upper and a lower bound, 80% and 20% for instance.
+    Starting empty, the battery will fill until at or above the upper threshold. It will then empty until at or below the lower threshold.
+    """
     def __init__(self, max_capacity = 1000):
         # Initialize with a capacity of zero
         self.capacity = 0
         # Set max capacity in kWh, default = 1000
         self.max_capacity = max_capacity
+        # A state used to check whether filling or emptying capacity
+        self.charging = True
+        # Upper and lower thresholds
+        self.upper_threshold = 0.8
+        self.lower_threshold = 0.2
 
 
     def make_bid(self):
-        # The Battery storage should generally buy if capacity is low and sell if capacity is high
-        # Logic sketch:
-        # 1. Start empty
-        # 2. Buy until capacity exceeds some threshold
-        # 3. Sell until below some other threshold
-        # 4. GOTO 2
         bids = []
-        # Pricing logic?
 
         action, quantity = self.make_prognosis(self)
-        bid = Bid(action=action, quantity=quantity, price = 0) # What should price be here?
+        if action is "Buy":
+            price = math.inf # Inf as upper bound for buying price
+        elif action is "Sell":
+            price = 0.0
+        bid = Bid(action=action, quantity=quantity, price=price) # What should price be here?
         # We need to express that the battery will buy at any price but prefers the lowest
         # And that it will buy up-to the specified amount but never more
        
@@ -76,12 +84,17 @@ class BatteryStorageAgent(IAgent):
 
     def make_prognosis(self):
         # Determine if we want to sell or buy
-        if self.capacity < 0.2*self.max_capacity:
+        if self.capacity < self.lower_threshold*self.max_capacity:
+            self.charging = True
+        elif self.capacity > self.upper_threshold*self.max_capacity:
+            self.charging = False
+            
+        
+        if self.charging:
             capacity_to_fill = self.max_capacity - self.capacity
             return "Buy", capacity_to_fill
-        elif self.capacity > 0.8*self.max_capacity:
+        else:
             return "Sell", self.capacity
-            
 
 
 class GridAgent(IAgent):
