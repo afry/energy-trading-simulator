3cf33ed27a1b92217b43e180c83e9152e2662fb8
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-11-03 11:43:22 +0100

    CLEANUP - changed nordpool_data from pd.DataFrame with 1 column, to pd.Series

--- data_store.py
+++ data_store.py
@@ -6,12 +6,12 @@ class DataStore:
 
     def __init__(self, external_price_csv='data/nordpool_area_grid_el_price.csv'):
         self.nordpool_data = pd.read_csv(external_price_csv, index_col=0)
-        if self.nordpool_data.mean()[0] > 100:
+        self.nordpool_data = self.nordpool_data.squeeze()
+        if self.nordpool_data.mean() > 100:
             # convert price from SEK per MWh to SEK per kWh
             self.nordpool_data = self.nordpool_data / 1000
-        self.nordpool_data.columns = ['price_sek_kwh']
 
         # Read more data here
 
     def get_nordpool_price_for_period(self, period):
-        return self.nordpool_data.loc[period].iloc[0]
+        return self.nordpool_data.loc[period]
