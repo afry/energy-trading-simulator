8268f0fd8fef0c6f58bf93a0e1cbdbeacbd4c4b3
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-11-05 10:04:19 +0100

    FEATURE - first implementation of a market solver

--- market_solver.py
+++ market_solver.py
@@ -1,7 +1,36 @@
-class MarketSolver():
-    """An entity that resolves bids towards the market in a way that fulfills the constraints of the bidding entities"""
-    
+from bid import Bid, Action
+from typing import List
 
-    def resolve_bids():
+
+class MarketSolver:
+    """An entity that resolves bids towards the market in a way that fulfills the constraints of the bidding entities"""
+
+    def resolve_bids(self, bids: List[Bid]):
         """Function for resolving all bids for the next trading period"""
-        pass
+
+        price_points = self.get_price_points(bids)
+
+        for price_point in sorted(price_points):
+            # Going through price points in ascending order
+            supply_for_price_point = 0
+            demand_for_price_point = 0
+            for bid in bids:
+                if bid.action == Action.SELL:
+                    if bid.price <= price_point:
+                        supply_for_price_point = supply_for_price_point + bid.quantity
+                else:  # BUY
+                    if bid.price >= price_point:
+                        demand_for_price_point = demand_for_price_point + bid.quantity
+
+            if supply_for_price_point >= demand_for_price_point:
+                # Found an acceptable price!
+                return price_point
+
+        raise Exception("No acceptable price found!")
+
+    @staticmethod
+    def get_price_points(bids):
+        price_points = set()
+        for bid in bids:
+            price_points.add(bid.price)
+        return price_points
---
+++ tests/test_market_solver.py
@@ -0,0 +1,41 @@
+import math
+from unittest import TestCase
+
+from bid import Bid, Action, Resource
+from market_solver import MarketSolver
+
+
+class TestMarketSolver(TestCase):
+
+    ms = MarketSolver()
+
+    def test_resolve_bids_1(self):
+        bids = [Bid(Action.SELL, Resource.ELECTRICITY, 100, 1),
+                Bid(Action.BUY, Resource.ELECTRICITY, 100, 1.5)]
+        # Someone willing to sell 100 kWh at 1 SEK/kWh,
+        # someone willing to buy 100 kWh at 1.5 SEK/kWh.
+        # Clearing price should be 1 SEK/kWh
+        clearing_price = self.ms.resolve_bids(bids)
+        self.assertEqual(1, clearing_price)
+
+    def test_resolve_bids_2(self):
+        bids = [Bid(Action.SELL, Resource.ELECTRICITY, 100, 1),
+                Bid(Action.BUY, Resource.ELECTRICITY, 100, 0.5)]
+        # Someone willing to sell 100 kWh at 1 SEK/kWh,
+        # someone willing to buy 100 kWh at 0.5 SEK/kWh.
+        # Clearing price should be 1 SEK/kWh
+        clearing_price = self.ms.resolve_bids(bids)
+        self.assertEqual(1, clearing_price)
+
+    def test_resolve_bids_3(self):
+        bids = [Bid(Action.SELL, Resource.ELECTRICITY, 100, 0),
+                Bid(Action.SELL, Resource.ELECTRICITY, 100, 1.5),
+                Bid(Action.SELL, Resource.ELECTRICITY, 10000, 10),
+                Bid(Action.BUY, Resource.ELECTRICITY, 200, math.inf)]
+        # Someone willing to sell 100 kWh at 0 SEK/kWh,
+        # someone willing to sell 100 kWh at 1.5 SEK/kWh,
+        # someone willing to sell 10000 kWh at 10 SEK/kWh,
+        # someone willing to buy 200 kWh at Inf SEK/kWh.
+        # Clearing price should be 1.5 SEK/kWh
+        clearing_price = self.ms.resolve_bids(bids)
+        self.assertEqual(1.5, clearing_price)
