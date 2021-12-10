75a6c778e6abf7aaf2959885c6860924d643aadd
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-12-10 14:58:14 +0100

    TEST - fixed E2E test when there is an agent who didn't trade, but were slapped with an "extra cost" in a trading period. For example, a PV agent who submitted a bid to sell electricity, but ended up producing 0 and not selling anything

--- tests/end_to_end_test.py
+++ tests/end_to_end_test.py
@@ -1,14 +1,16 @@
 
 from unittest import TestCase
 
-from tradingplatformpoc import main
+from datetime import datetime
+
+from tradingplatformpoc import simulation_runner
 from tradingplatformpoc.bid import Action
 
 
 class Test(TestCase):
 
     def test(self):
-        clearing_prices, all_trades, all_extra_costs = main.run_trading_simulations()
+        clearing_prices, all_trades, all_extra_costs = simulation_runner.run_trading_simulations()
 
         for period in clearing_prices.keys():
             trades_for_period = [x for x in all_trades if x.period == period]
@@ -18,8 +20,9 @@ class Test(TestCase):
             self.assertAlmostEqual(elec_bought_kwh, elec_sold_kwh, places=7)
 
             total_cost = 0  # Should sum to 0 at the end of this loop
-            agents_who_traded = set([x.source for x in trades_for_period])
-            for agent in agents_who_traded:
+            agents_who_traded_or_were_penalized = set([x.source for x in trades_for_period] +
+                                                      list(extra_costs_for_period.keys()))
+            for agent in agents_who_traded_or_were_penalized:
                 trades_for_agent = [x for x in trades_for_period if x.source == agent]
                 extra_costs_for_agent = get_extra_cost_for_agent(extra_costs_for_period, agent)
                 cost_for_agent = sum([x.get_cost_of_trade() for x in trades_for_agent]) + extra_costs_for_agent
