ac3ba880762dabf954df17b236d4f66c3bf61d82
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2021-12-21 13:55:21 +0100

    FEATURE - when running main, one can optionally add the console logging level as an argument

--- tradingplatformpoc/main.py
+++ tradingplatformpoc/main.py
@@ -1,13 +1,28 @@
 from tradingplatformpoc.simulation_runner import run_trading_simulations
 import logging
+import sys
+
+# --- Read sys.argv to get logging level, if it is specified ---
+string_to_log_later = None
+if len(sys.argv) > 1 and type(sys.argv[1]) == str:
+    arg_to_upper = str.upper(sys.argv[1])
+    try:
+        console_log_level = getattr(logging, arg_to_upper)
+    except AttributeError:
+        # Since we haven't set up the logger yet, will store this message and log it a little bit further down.
+        string_to_log_later = "No logging level found with name '{}', console logging level will default to INFO.".\
+            format(arg_to_upper)
+        console_log_level = logging.INFO
+else:
+    console_log_level = logging.INFO
 
 # --- Format logger for print statements
 FORMAT = "%(asctime)-15s | %(levelname)-7s | %(name)-35.35s | %(message)s"
 
 file_handler = logging.FileHandler("../trading-platform-poc.log")
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG)  # File logging always DEBUG
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(console_log_level)
 
 logging.basicConfig(
     level=logging.DEBUG, format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S",
@@ -15,6 +30,9 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+if string_to_log_later is not None:
+    logger.info(string_to_log_later)
 
 if __name__ == '__main__':
     logger.info("Running main")
