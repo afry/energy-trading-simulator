ed3c8718ca89f851767eb8c4e3db2b82f0c2e179
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2022-01-11 16:30:01 +0100

    CLEANUP - did a little hack to ensure that end_to_end_test.py can run both from the "tests" directory and from the base directory

--- tests/end_to_end_test.py
+++ tests/end_to_end_test.py
@@ -1,4 +1,4 @@
-
+import os
 from unittest import TestCase
 
 from tradingplatformpoc import simulation_runner
@@ -8,8 +8,10 @@ from tradingplatformpoc.bid import Action
 class Test(TestCase):
 
     def test(self):
-        clearing_prices, all_trades, all_extra_costs = simulation_runner.run_trading_simulations(
-            '../tradingplatformpoc/data/generated/mock_datas.pickle')
+        current_directory = os.getcwd()
+        mock_datas_file_path = get_mock_datas_pickle_file_path(current_directory)
+
+        clearing_prices, all_trades, all_extra_costs = simulation_runner.run_trading_simulations(mock_datas_file_path)
 
         for period in clearing_prices.keys():
             trades_for_period = [x for x in all_trades if x.period == period]
@@ -27,6 +29,17 @@ class Test(TestCase):
                 cost_for_agent = sum([x.get_cost_of_trade() for x in trades_for_agent]) + extra_costs_for_agent
                 total_cost = total_cost + cost_for_agent
             self.assertAlmostEqual(0, total_cost)
+
+
+def get_mock_datas_pickle_file_path(current_directory):
+    folders = current_directory.split("\\")
+    if folders[len(folders) - 1] == 'trading-platform-poc':
+        mock_datas_file_path = './tradingplatformpoc/data/generated/mock_datas.pickle'
+    elif folders[len(folders) - 2] == 'trading-platform-poc':
+        mock_datas_file_path = '../tradingplatformpoc/data/generated/mock_datas.pickle'
+    else:
+        raise RuntimeError('Could not find pickle file with mock data')
+    return mock_datas_file_path
 
 
 def get_extra_cost_for_agent(extra_costs_for_period, agent):
