85d55b62ac9474ba5107952a9f99c09215b38d09
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2022-01-12 11:47:29 +0100

    TEST - tested new functionality in market solver, checking so that returned bids have acceptance status set, and that their values correspond to what is expected

--- tests/test_market_solver.py
+++ tests/test_market_solver.py
@@ -18,9 +18,12 @@ class TestMarketSolver(TestCase):
         # Clearing price should be 1 SEK/kWh
         clearing_price, bids_with_acceptance_status = self.ms.resolve_bids(bids)
         self.assertEqual(1, clearing_price)
+        for bid in bids_with_acceptance_status:
+            self.assertIsNotNone(bid.was_accepted)
+            self.assertTrue(bid.was_accepted)
 
     def test_resolve_bids_2(self):
-        """Test the clearing price calculation when there are no accepted bids."""
+        """Test the clearing price calculation when there are no accepted bids on one side (buy-side)."""
         bids = [Bid(Action.SELL, Resource.ELECTRICITY, 100, 1, "Seller1", False),
                 Bid(Action.BUY, Resource.ELECTRICITY, 100, 0.5, "Buyer1", False)]
         # Someone willing to sell 100 kWh at 1 SEK/kWh,
@@ -28,20 +31,28 @@ class TestMarketSolver(TestCase):
         # Clearing price should be 1 SEK/kWh
         clearing_price, bids_with_acceptance_status = self.ms.resolve_bids(bids)
         self.assertEqual(1, clearing_price)
+        for bid in bids_with_acceptance_status:
+            self.assertIsNotNone(bid.was_accepted)
+            if bid.source == 'Buyer1':
+                self.assertFalse(bid.was_accepted)
+            else:
+                self.assertTrue(bid.was_accepted)
 
     def test_resolve_bids_3(self):
         """Test the clearing price calculation when there are 4 different bids, one of them from an external grid."""
         bids = [Bid(Action.SELL, Resource.ELECTRICITY, 100, 0, "Seller1", False),
                 Bid(Action.SELL, Resource.ELECTRICITY, 100, 1.5, "Seller2", False),
                 Bid(Action.SELL, Resource.ELECTRICITY, 10000, 10, "Grid", True),
-                Bid(Action.BUY, Resource.ELECTRICITY, 200, math.inf, "Buyer1", False)]
+                Bid(Action.BUY, Resource.ELECTRICITY, 300, math.inf, "Buyer1", False)]
         # Someone willing to sell 100 kWh at 0 SEK/kWh,
         # someone willing to sell 100 kWh at 1.5 SEK/kWh,
         # someone willing to sell 10000 kWh at 10 SEK/kWh,
-        # someone willing to buy 200 kWh at Inf SEK/kWh.
-        # Clearing price should be 1.5 SEK/kWh
+        # someone willing to buy 300 kWh at Inf SEK/kWh.
+        # Clearing price should be 10 SEK/kWh
         clearing_price, bids_with_acceptance_status = self.ms.resolve_bids(bids)
-        self.assertEqual(1.5, clearing_price)
+        self.assertEqual(10, clearing_price)
+        for bid in bids_with_acceptance_status:
+            self.assertTrue(bid.was_accepted)
 
     def test_resolve_bids_4(self):
         """Test the clearing price calculation when there are 4 different bids, and the locally produced energy covers
@@ -57,6 +68,12 @@ class TestMarketSolver(TestCase):
         # Clearing price should be 1 SEK/kWh
         clearing_price, bids_with_acceptance_status = self.ms.resolve_bids(bids)
         self.assertEqual(1, clearing_price)
+        for bid in bids_with_acceptance_status:
+            self.assertIsNotNone(bid.was_accepted)
+            if bid.source == 'Grid':
+                self.assertFalse(bid.was_accepted)
+            else:
+                self.assertTrue(bid.was_accepted)
 
     def test_resolve_bids_5(self):
         """Test that an Error is raised when there isn't enough energy to satisfy the local demand."""
@@ -76,3 +93,9 @@ class TestMarketSolver(TestCase):
         # Clearing price should be 0.46069 SEK/kWh
         clearing_price, bids_with_acceptance_status = self.ms.resolve_bids(bids)
         self.assertEqual(0.46069, clearing_price)
+        for bid in bids_with_acceptance_status:
+            self.assertIsNotNone(bid.was_accepted)
+            if bid.source == 'ElectricityGridAgent':
+                self.assertFalse(bid.was_accepted)
+            else:
+                self.assertTrue(bid.was_accepted)
