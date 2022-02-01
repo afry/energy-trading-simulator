8a5def83f580cf961bc715383cc5e3e966e86ed3
AhmadOstovar <ahmad.ostovar@afry.com>
2022-02-01 11:54:47 +0100

    tested with different values, additional comments are added and values are changed to pass tests

--- tests/test_storage_digital_twin.py
+++ tests/test_storage_digital_twin.py
@@ -6,20 +6,21 @@ from tradingplatformpoc.digitaltwin.storage_digital_twin import StorageDigitalTw
 class TestStorageDigitalTwin(TestCase):
 
     storage_digital_twin = StorageDigitalTwin(max_capacity_kwh=100.0, max_charge_rate_fraction=0.25,
-                                              max_discharge_rate_fraction=0.2, start_capacity_kwh=6.0,
+                                              max_discharge_rate_fraction=0.2, start_capacity_kwh=6,
                                               discharging_efficiency=0.93)
 
     def test_charge_battery(self):
         """Test charging the battery with the input amount. It will charge the battery with
             maximum amount of (max_capacity_kwh * max_charge_rate_fraction), if the capacity (after charging)
             is equal or less than "max_capacity_kwh", at one time step."""
-        self.assertAlmostEqual(5, self.storage_digital_twin.charge(5.0))
+        self.assertAlmostEqual(5, self.storage_digital_twin.charge(5))
 
     def test_discharge_battery(self):
         """Test discharging the battery with the input amount. Total discharging amount should be equal or less than
             (max_capacity_kwh * max_discharge_rate_fraction), if it does not exceed (capacity * discharging_efficiency),
-             at one time step."""
-        self.assertAlmostEqual(5, self.storage_digital_twin.discharge(5.0))
+             at one time step. When discharging the storage X kWh, the current capacity decreased by
+             (X / discharging_efficiency)."""
+        self.assertAlmostEqual(4.65, self.storage_digital_twin.discharge(4.65))
 
     def test_possible_charge_amount(self):
         self.assertAlmostEqual(25, self.storage_digital_twin.get_possible_charge_amount())
