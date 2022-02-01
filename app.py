a19d63522daf0873416e27679c3fec747488c06e
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2022-02-01 12:22:37 +0100

    UI-FEATURE - multi-page - added another page where one can look at all bids for a given agent

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pkg_resources import resource_filename
+from streamlit.state.session_state import SessionState
 
 from tradingplatformpoc import data_store
 from tradingplatformpoc.simulation_runner import run_trading_simulations
@@ -14,6 +15,8 @@ import altair as alt
 # Note: To debug a streamlit script, see https://stackoverflow.com/a/60172283
 # This would be neat, but haven't been able to get it to work
 # https://altair-viz.github.io/altair-tutorial/notebooks/06-Selections.html#binding-scales-to-other-domains
+START_PAGE = "Start page"
+BIDS_PAGE = "Bids"
 WHOLESALE_PRICE_STR = 'External wholesale price'
 RETAIL_PRICE_STR = 'External retail price'
 LOCAL_PRICE_STR = 'Local price'
@@ -35,10 +38,11 @@ def load_data():
     prices_df[WHOLESALE_PRICE_STR] = prices_df['nordpool'] + data_store.ELECTRICITY_WHOLESALE_PRICE_OFFSET
     prices_df.drop(['nordpool'], axis=1, inplace=True)
 
-    clearing_prices_df = pd.read_csv("clearing_prices.csv")
-    clearing_prices_df['period'] = pd.to_datetime(clearing_prices_df['period'])
+    all_bids = pd.read_csv("bids.csv")
+    all_bids['period'] = pd.to_datetime(all_bids['period'])
+    all_bids.drop(['by_external'], axis=1, inplace=True)  # Don't need this column
 
-    return prices_df
+    return prices_df, all_bids
 
 
 # --- Read sys.argv to get logging level, if it is specified ---
@@ -90,6 +94,17 @@ def construct_price_chart(prices_df: pd.DataFrame):
         interactive(bind_y=False)
 
 
+def get_price_df_when_local_price_inbetween(prices_df: pd.DataFrame) -> pd.DataFrame:
+    """Local price is almost always either equal to the external wholesale or retail price. This method returns the
+    subsection of the prices dataframe where the local price is _not_ equal to either of these two."""
+    price_df_dt_index = prices_df.set_index("period")
+    local_price_between_external = (price_df_dt_index[LOCAL_PRICE_STR]
+                                    > price_df_dt_index[WHOLESALE_PRICE_STR]
+                                    + 0.0001) & (price_df_dt_index[LOCAL_PRICE_STR]
+                                                 < price_df_dt_index[RETAIL_PRICE_STR] - 0.0001)
+    return price_df_dt_index.loc[local_price_between_external]
+
+
 if __name__ == '__main__':
     st.write(
         """
@@ -100,38 +115,44 @@ if __name__ == '__main__':
     )
 
     st.sidebar.write("""
-    # This is a sidebar where we can have navigation options
+    # Navigation
     """)
 
-    # These options in the sidebar combined with if clauses can be used to build a
-    # multi-page app for more advanced interactions, like switching between experiments
-    # or upload/run/analysis pages
+    page_sel_placeholder = st.sidebar.empty()
+    # Will be disabled on startup, and enabled once data has been loaded
+    page_selected = page_sel_placeholder.radio("Select page to view", (START_PAGE, BIDS_PAGE),
+                                               disabled='combined_price_df' not in st.session_state)
 
-    selection = st.sidebar.selectbox("Options", ("Option 1", "Option 2", "Option 3"))
+    if page_selected == START_PAGE:
+        run_sim = st.button("Click here to run simulation")
+        if run_sim:
+            run_sim = False
+            logger.info("Running simulation")
+            st.spinner("Running simulation")
+            clearing_prices_dict, all_trades_list, all_extra_costs_dict = run_trading_simulations(mock_datas_path)
+            st.success('Simulation finished!')
 
-    radio_selection = st.sidebar.radio("Radio options", ("radio 1", "radio 2"))
+        data_button = st.button("Click here to load data")
+        if data_button:
+            data_button = False
+            logger.info("Loading data")
+            st.spinner("Loading data")
+            combined_price_df, bids_df = load_data()
+            st.session_state.combined_price_df = combined_price_df
+            st.session_state.bids_df = bids_df
+            st.session_state.agents_sorted = sorted(bids_df.agent.unique())
+            st.success("Data loaded!")
+            page_selected = page_sel_placeholder.radio("Select page to view", (START_PAGE, BIDS_PAGE), disabled=False)
 
-    run_sim = st.button("Click here to run simulation")
-    if run_sim:
-        run_sim = False
-        logger.info("Running simulation")
-        st.spinner("Running simulation")
-        clearing_prices_dict, all_trades_list, all_extra_costs_dict = run_trading_simulations(mock_datas_path)
-        st.success('Simulation finished!')
+            price_chart = construct_price_chart(combined_price_df)
 
-    data_button = st.button("Click here to load data")
-    if data_button:
-        data_button = False
-        logger.info("Loading data")
-        st.spinner("Loading data")
-        combined_price_df = load_data()
-        st.success("Data loaded!")
+            st.session_state.price_chart = price_chart
 
-        chart = construct_price_chart(combined_price_df)
+        if 'price_chart' in st.session_state:
+            st.altair_chart(st.session_state.price_chart, use_container_width=True)
 
-        st.altair_chart(chart, use_container_width=True)
+    elif page_selected == BIDS_PAGE:
+        agent_chosen = st.selectbox(label='Choose agent', options=st.session_state.agents_sorted)
 
-        price_df_dt_index = combined_price_df.set_index("period")
-        local_price_between_external = (price_df_dt_index[LOCAL_PRICE_STR] > price_df_dt_index[WHOLESALE_PRICE_STR] + 0.0001) & (
-                    price_df_dt_index[LOCAL_PRICE_STR] < price_df_dt_index[RETAIL_PRICE_STR] - 0.0001)
-        st.dataframe(price_df_dt_index.loc[local_price_between_external])
+        st.dataframe(st.session_state.bids_df.loc[st.session_state.bids_df.agent == agent_chosen].
+                     drop(['agent'], axis=1))
