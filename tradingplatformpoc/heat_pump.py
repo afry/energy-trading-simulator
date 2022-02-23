5b3200ccd4b0e4018052a142f60034339b98af33
Liam Håkansson <liam.hakansson@afry.com>
2022-02-23 11:34:13 +0100

    test

--- tradingplatformpoc/heat_pump.py
+++ tradingplatformpoc/heat_pump.py
@@ -13,4 +13,6 @@ class HeatPump():
 
     Given the forecasted efficiency, the BuildingAgent should optimise the mix of heat pump/district heat with respect to total cost,
     where the cost may or may not be adjusted for carbon intensity.
+
+    The function for calculating pump efficiency based on gear and desired temperature can be based on the work in the Decision maker project
     """
