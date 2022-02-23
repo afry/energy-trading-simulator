5906fc046f4c1bf04bc0b28bf8bab324866cb53f
= <=>
2022-02-23 08:34:10 +0100

    Added a file for heat pump component

---
+++ tradingplatformpoc/heat_pump.py
@@ -0,0 +1,16 @@
+class HeatPump():
+    """
+    A component to allow building agents to convert electricity to heat.
+    """
+    coeff_of_perf: float
+    max_capacity: float
+
+    """
+    This class needs the following:
+    
+    1. A function for calculating forcasted efficiency att different gears given the brine and target temperature
+    2. A method for actually converting power to heat at the specified efficiency
+
+    Given the forecasted efficiency, the BuildingAgent should optimise the mix of heat pump/district heat with respect to total cost,
+    where the cost may or may not be adjusted for carbon intensity.
+    """
