befa074424ba4d93f6536d7f1574c038b4464067
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2022-03-01 13:44:45 +0100

    FEATURE - suggested new heat pump implementation. Models are a lot simpler. Added method to go through all possible workloads and calculate the input electricity, and output heating. An agent can use this function to figure out what it wants to do with its heat pump(s)

--- tests/test_heat_pump.py
+++ tests/test_heat_pump.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 
-import numpy as np
-
 from tradingplatformpoc import heat_pump
 
 
@@ -10,22 +8,27 @@ class Test(TestCase):
     def test_throughput_calculation(self):
         test_pump = heat_pump.HeatPump()
 
-        forward_temp_c = np.array([45])
-        workload = np.array([60])
-        brine_temp_c = np.array([0])
+        elec_input, heat_output = test_pump.calculate_energy(workload=6, forward_temp_c=60, brine_temp_c=0)
+        cop_output = heat_output / elec_input
+        self.assertAlmostEqual(2.7613787873898135, cop_output)
 
-        heat_output, cop_output = test_pump.get_heatpump_throughputs(forward_temp_c=forward_temp_c, workload=workload,
-                                                                     brine_temp_c=brine_temp_c)
-        print(heat_output, cop_output)
-        self.assertAlmostEqual(heat_output[0], 28.8, places=1)
-        self.assertAlmostEqual(cop_output[0], 5.9, places=1)
+        # If we want a "better" heat pump, assert that output COP increases by the correct amount
+        better_pump = heat_pump.HeatPump(5)
 
-    def test_power_conversion(self):
+        elec_input, heat_output = better_pump.calculate_energy(workload=6, forward_temp_c=60, brine_temp_c=0)
+        better_cop_output = heat_output / elec_input
+        cop_output_percent_increase = better_cop_output / cop_output
+        cop_input_percent_increase = 5 / heat_pump.DEFAULT_COP
+        self.assertAlmostEqual(cop_input_percent_increase, cop_output_percent_increase)
+
+    def test_calculate_for_all_workloads(self):
         test_pump = heat_pump.HeatPump()
 
-        input_power = 100
-        cop = 3.5
+        results = test_pump.calculate_for_all_workloads()
 
-        heat_output = test_pump.produce_heat(input_elec_power=input_power, cop=cop)
+        self.assertEqual(11, len(results.index))
 
-        self.assertEqual(heat_output, input_power * cop)
+        results = results.sort_values(by=['workload'], axis=0, ascending=True)
+        # When sorted by workload, both input and output should be steadily increasing
+        self.assertTrue(results['input'].is_monotonic_increasing)
+        self.assertTrue(results['output'].is_monotonic_increasing)
