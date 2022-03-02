f8a121c4ab7be0c753fde6ed0aa104178df73564
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2022-03-02 11:22:55 +0100

    Added documentation and tests for heat pump

--- tradingplatformpoc/heat_pump.py
+++ tradingplatformpoc/heat_pump.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Tuple
 
 import numpy as np
@@ -25,6 +26,9 @@ RPM_MAX = 6000
 MIN_WORKLOAD = 1
 MAX_WORKLOAD = 10
 POSSIBLE_WORKLOADS_WHEN_RUNNING: List[int] = np.arange(MIN_WORKLOAD, MAX_WORKLOAD + 1).tolist()
+
+
+logger = logging.getLogger(__name__)
 
 
 class HeatPump:
@@ -73,7 +77,7 @@ class HeatPump:
     def calculate_for_all_workloads(self, forward_temp_c: float = DEFAULT_FORWARD_TEMP,
                                     brine_temp_c: float = DEFAULT_BRINE_TEMP) -> pd.DataFrame:
         """
-        Returns workload, electricity needed, heating produced
+        Returns a pd.DataFrame with workload, electricity needed, heating produced
         """
         # Want to evaluate all possible gears, and also to not run the heat pump at all
         workloads: List[int] = [0] + POSSIBLE_WORKLOADS_WHEN_RUNNING
@@ -96,6 +100,28 @@ class ValueOutOfRangeError(Exception):
 
 
 def model_elec_needed(forward_temp_c: float, rpm: float) -> float:
+    """
+    This method calculates the electricity needed (in kW) for running the heat pump at the given RPM and forward
+    temperature.
+    Background: All data we had were a sparse table, provided by the heat pump manufacturer (Thermia), of electricity
+    needed for some different RPMs, forward temperatures, and brine temperatures. From this table, we constructed a
+    simple regression model, so that we can calculate it for more values than just the ones in the table.
+    This work is done in "simple_heat_pump_model.ipynb" in data-exploration project.
+
+    @param forward_temp_c: A float describing the forward temperature in degrees Celsius. In the data used to fit this
+        model we only had two unique values of this parameter: 35 and 55. Therefore this method will log a warning if
+        this parameter is < 30 or > 60.
+    @param rpm: A float describing the revolutions per minute. In the data used to fit this model, this parameter ranged
+        between 1500 and 6000. Therefore this method will log a warning if this parameter is < 1000 or > 7000.
+
+    @return An estimate of the electricity needed, in kW, to run the heat pump with the given settings
+    """
+    if forward_temp_c < 30 or forward_temp_c > 60:
+        logger.warning("Heat pump electricity consumption model was fit with forward temperature values of 35 and 55, "
+                       "but got {} as input!".format(forward_temp_c))
+    if rpm < 1000 or rpm > 7000:
+        logger.warning("Heat pump electricity consumption model was fit with RPM values from 1500 to 6000, "
+                       "but got {} as input!".format(rpm))
     return ELEC_INTERCEPT_COEF + \
         ELEC_RPM_SQUARED_COEF * rpm * rpm + \
         ELEC_FORWARD_TEMP_COEF * forward_temp_c + \
@@ -103,6 +129,34 @@ def model_elec_needed(forward_temp_c: float, rpm: float) -> float:
 
 
 def model_heat_output(forward_temp_c: float, rpm: float, brine_temp_c: float) -> float:
+    """
+    This method calculates the heat produced (in kW) when running the heat pump at the given RPM and forward
+    temperature.
+    Background: All data we had were a sparse table, provided by the heat pump manufacturer (Thermia), of heat produced
+    when running the heat pump for some different RPMs, forward temperatures, and brine temperatures. From this table,
+    we constructed a simple regression model, so that we can calculate it for more values than just the ones in the
+    table. This work is done in "simple_heat_pump_model.ipynb" in data-exploration project.
+
+    @param forward_temp_c: A float describing the forward temperature in degrees Celsius. In the data used to fit this
+        model we only had two unique values of this parameter: 35 and 55. Therefore this method will log a warning if
+        this parameter is < 30 or > 60.
+    @param rpm: A float describing the revolutions per minute. In the data used to fit this model, this parameter ranged
+        between 1500 and 6000. Therefore this method will log a warning if this parameter is < 1000 or > 7000.
+    @param brine_temp_c: A float describing the brine fluid temperature in degrees Celsius. In the data used to fit this
+        model we only had 3 unique values of this parameter: -5, 0 and 5. Therefore this method will log a warning if
+        this parameter is < -10 or > 10.
+
+    @return An estimate of the heat produced, in kW, to run the heat pump with the given settings
+    """
+    if forward_temp_c < 30 or forward_temp_c > 60:
+        logger.warning("Heat pump electricity consumption model was fit with forward temperature values of 35 and 55, "
+                       "but got {} as input!".format(forward_temp_c))
+    if rpm < 1000 or rpm > 7000:
+        logger.warning("Heat pump electricity consumption model was fit with RPM values from 1500 to 6000, "
+                       "but got {} as input!".format(rpm))
+    if brine_temp_c < -10 or brine_temp_c > 10:
+        logger.warning("Heat pump electricity consumption model was fit with brine temperature values of -5, 0 and 5, "
+                       "but got {} as input!".format(brine_temp_c))
     return HEAT_INTERCEPT_COEF + \
         HEAT_RPM_COEF * rpm + \
         HEAT_FORWARD_TEMP_TIMES_RPM_COEF * forward_temp_c * rpm + \
