69ffdd94e0beacef8c075fbf8a54a217dceeed47
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2023-02-09 11:16:42 +0100

    UI - small change to footer

--- tradingplatformpoc/app/footer.py
+++ tradingplatformpoc/app/footer.py
@@ -37,7 +37,8 @@ text-align: left; /* you can replace 'left' with 'center' or 'right' if you want
 <div id="page-container">
 
 <div class="footer">
-Version 1.0.0
+Version 1.0.0<br>
+Built by AFRY X - AI & Advanced Analytics
 </div>
 
 </div>
