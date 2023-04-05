1c8c1821a40b15615f3c56233491ffe784df8ded
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-04-05 16:06:26 +0200

    Adding compressed version of model

---
+++ tradingplatformpoc/compress.py
@@ -0,0 +1,15 @@
+import bz2file as bz2
+import pickle
+
+
+def bz2_compressed_pickle(title, data):
+
+    with bz2.BZ2File(title, 'w') as f:
+        pickle.dump(data, f)
+
+
+def bz2_decompress_pickle(file):
+
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
