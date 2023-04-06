86afafc613460bf40101c4c508044744fca4f9d5
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-04-06 11:42:03 +0200

    Remove unused function

--- tradingplatformpoc/compress.py
+++ tradingplatformpoc/compress.py
@@ -3,12 +3,6 @@ import pickle
 import bz2file as bz2
 
 
-def bz2_compressed_pickle(title, data):
-
-    with bz2.BZ2File(title, 'w') as f:
-        pickle.dump(data, f)
-
-
 def bz2_decompress_pickle(file):
 
     data = bz2.BZ2File(file, 'rb')
