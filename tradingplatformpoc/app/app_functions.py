df24abd2c5d61355995787ab91b97f48d28d658e
Porter <camille.porter@afry.com>
2023-06-05 09:10:51 +0200

    added titles to charts and descriptions

--- tradingplatformpoc/app/app_functions.py
+++ tradingplatformpoc/app/app_functions.py
@@ -44,18 +44,22 @@ def construct_price_chart(prices_df: pd.DataFrame, resource: Resource) -> alt.Ch
     domain = [app_constants.LOCAL_PRICE_STR, app_constants.RETAIL_PRICE_STR, app_constants.WHOLESALE_PRICE_STR]
     range_color = ['blue', 'green', 'red']
     range_dash = [[0, 0], [2, 4], [2, 4]]
-    return alt.Chart(data_to_use).mark_line(). \
+    title = alt.TitleParams("Total of locally produced heating and electricity", anchor='middle')
+    selection = alt.selection_single(fields=['variable'], bind='legend')
+    return alt.Chart(data_to_use, title=title).mark_line(). \
         encode(x=alt.X('period', axis=alt.Axis(title='Period (UTC)'), scale=alt.Scale(type="utc")),
                y=alt.Y('value', axis=alt.Axis(title='Price [SEK]')),
                color=alt.Color('variable', scale=alt.Scale(domain=domain, range=range_color)),
                strokeDash=alt.StrokeDash('variable', scale=alt.Scale(domain=domain, range=range_dash)),
+               opacity=alt.condition(selection, alt.value(1), alt.value(0.2)),
                tooltip=[alt.Tooltip(field='period', title='Period', type='temporal', format='%Y-%m-%d %H:%M'),
                         alt.Tooltip(field='variable', title='Variable'),
                         alt.Tooltip(field='value', title='Value')]). \
-        interactive(bind_y=False)
+        add_selection(selection).interactive(bind_y=False)
 
 
-def construct_static_digital_twin_chart(digital_twin: StaticDigitalTwin, should_add_hp_to_legend: bool = False) -> \
+def construct_static_digital_twin_chart(digital_twin: StaticDigitalTwin, agent_chosen_guid: str,
+                                        should_add_hp_to_legend: bool = False) -> \
         alt.Chart:
     """
     Constructs a multi-line chart from a StaticDigitalTwin, containing all data held therein.
@@ -92,11 +96,12 @@ def construct_static_digital_twin_chart(digital_twin: StaticDigitalTwin, should_
     if should_add_hp_to_legend:
         domain.append('Heat pump workload')
         range_color.append(app_constants.HEAT_PUMP_CHART_COLOR)
-    return altair_period_chart(df, domain, range_color)
+    return altair_period_chart(df, domain, range_color, agent_chosen_guid)
 
 
 def construct_building_with_heat_pump_chart(agent_chosen: Union[BuildingAgent, PVAgent],
-                                            heat_pump_levels_dict: Dict[str, Dict[datetime.datetime, float]]) -> \
+                                            heat_pump_levels_dict: Dict[str, Dict[datetime.datetime, float]],
+                                            agent_chosen_guid: str) -> \
         alt.Chart:
     """
     Constructs a multi-line chart with energy production/consumption levels, with any heat pump workload data in the
@@ -105,7 +110,7 @@ def construct_building_with_heat_pump_chart(agent_chosen: Union[BuildingAgent, P
 
     heat_pump_data = heat_pump_levels_dict.get(agent_chosen.guid, {})
     if heat_pump_data == {}:
-        return construct_static_digital_twin_chart(agent_chosen.digital_twin, False)
+        return construct_static_digital_twin_chart(agent_chosen.digital_twin, agent_chosen_guid, False)
 
     st.write('Note: Energy production/consumption values do not include production/consumption by the heat pumps.')
     heat_pump_df = pd.DataFrame.from_dict(heat_pump_data, orient='index').reset_index()
@@ -119,7 +124,7 @@ def construct_building_with_heat_pump_chart(agent_chosen: Union[BuildingAgent, P
                  alt.Tooltip(field='Heat pump workload', title='Heat pump workload', type='quantitative')]
     )
 
-    energy_multiline = construct_static_digital_twin_chart(agent_chosen.digital_twin, True)
+    energy_multiline = construct_static_digital_twin_chart(agent_chosen.digital_twin, agent_chosen_guid, True)
     return alt.layer(heat_pump_area, energy_multiline).resolve_scale(y='independent')
 
 
@@ -780,13 +785,15 @@ def construct_traded_amount_by_agent_chart(agent_chosen_guid: str,
                                           'value': 0.0,
                                           'variable': elem['title']})))
 
-    return altair_period_chart(df, domain, range_color)
+    return altair_period_chart(df, domain, range_color, agent_chosen_guid)
 
 
-def altair_period_chart(df: pd.DataFrame, domain: List[str], range_color: List[str]) -> alt.Chart:
+def altair_period_chart(df: pd.DataFrame, domain: List[str], range_color: List[str],
+                        agent_chosen_guid: str) -> alt.Chart:
     """Altair chart for one or more variables over period."""
     selection = alt.selection_single(fields=['variable'], bind='legend')
-    return alt.Chart(df).mark_line(). \
+    title = alt.TitleParams('Energy and Heating Production/Consumption for ' + agent_chosen_guid, anchor='middle')
+    return alt.Chart(df, title=title).mark_line(). \
         encode(x=alt.X('period:T', axis=alt.Axis(title='Period (UTC)'), scale=alt.Scale(type="utc")),
                y=alt.Y('value', axis=alt.Axis(title='Energy [kWh]')),
                color=alt.Color('variable', scale=alt.Scale(domain=domain, range=range_color)),
