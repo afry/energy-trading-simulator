e865c215a4ca621fbcf245b39d19ec98929569f7
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-08-17 10:15:23 +0200

    Update with missing tables to delete_job and deletion of unused delete_from_db

--- tradingplatformpoc/database.py
+++ tradingplatformpoc/database.py
@@ -2,16 +2,11 @@ import logging
 from contextlib import _GeneratorContextManager
 from typing import Callable
 
-from sqlalchemy import delete
-
 from sqlmodel import SQLModel, Session
 
 from tradingplatformpoc.config.access_config import read_config
 from tradingplatformpoc.connection import db_engine, session_scope
-from tradingplatformpoc.sql.bid.models import Bid as TableBid
 from tradingplatformpoc.sql.config.crud import create_config_if_not_in_db
-from tradingplatformpoc.sql.extra_cost.models import ExtraCost as TableExtraCost
-from tradingplatformpoc.sql.trade.models import Trade as TableTrade
 
 logger = logging.getLogger(__name__)
 
@@ -30,18 +25,6 @@ def bulk_insert(objects: list,
                 session_generator: Callable[[], _GeneratorContextManager[Session]] = session_scope):
     with session_generator() as db:
         db.bulk_save_objects(objects)
-        db.commit()
-
-
-def delete_from_db(job_id: str, table_name: str,
-                   session_generator: Callable[[], _GeneratorContextManager[Session]] = session_scope):
-    with session_generator() as db:
-        if table_name == 'Trade':
-            db.execute(delete(TableTrade).where(TableTrade.job_id == job_id))
-        elif table_name == 'Bid':
-            db.execute(delete(TableBid).where(TableBid.job_id == job_id))
-        elif table_name == 'ExtraCost':
-            db.execute(delete(TableExtraCost).where(TableExtraCost.job_id == job_id))
         db.commit()
 
 
