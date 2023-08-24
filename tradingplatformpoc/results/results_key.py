ff966442d46920439462df4dec7f1b13614b69d7
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-08-24 15:26:19 +0200

    Chaning before to after in ResultsKey enum for net savings

--- tradingplatformpoc/results/results_key.py
+++ tradingplatformpoc/results/results_key.py
@@ -4,8 +4,8 @@ from enum import Enum
 class ResultsKey(Enum):
     SAVING_ABS_GROSS = "Saving by using local market, before taking penalties into account [SEK]"
     SAVING_REL_GROSS = "Saving by using local market, before taking penalties into account [%]"
-    SAVING_ABS_NET = "Saving by using local market, before taking penalties into account [SEK]"
-    SAVING_REL_NET = "Saving by using local market, before taking penalties into account [%]"
+    SAVING_ABS_NET = "Saving by using local market, after taking penalties into account [SEK]"
+    SAVING_REL_NET = "Saving by using local market, after taking penalties into account [%]"
     PENALTIES_BID_INACCURACY = "Total penalties accrued for bid inaccuracies [SEK]"
     ELEC_BOUGHT = "Electricity bought [kWh]"
     HEAT_BOUGHT = "Heating bought [kWh]"
