d2a8f6d43a9b4baac3dfb15ee60cdc8513421bb5
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-08-28 16:29:44 +0200

    Delete outdated results file functions

--- tradingplatformpoc/app/app_functions.py
+++ tradingplatformpoc/app/app_functions.py
@@ -1,51 +1,16 @@
-import datetime
+
 import logging
-import os
-import pickle
 from typing import Tuple
 
 import pandas as pd
 
 import streamlit as st
 
-from tradingplatformpoc.app import app_constants
-from tradingplatformpoc.compress import bz2_compress_pickle, bz2_decompress_pickle
 from tradingplatformpoc.constants import MOCK_DATA_PATH
-from tradingplatformpoc.results.simulation_results import SimulationResults
 from tradingplatformpoc.simulation_runner.trading_simulator import TradingSimulator
 
 
 logger = logging.getLogger(__name__)
-
-
-# ------------------------------------- Save result functions ---------------------------------
-def set_simulation_results(simulation_results: SimulationResults):
-    """Writes simulation results to file."""
-    data = (datetime.datetime.now(datetime.timezone.utc), simulation_results)
-    bz2_compress_pickle(app_constants.LAST_SIMULATION_RESULTS, data)
-
-
-def read_simulation_results() -> Tuple[datetime.datetime, SimulationResults]:
-    """Reads simulation results from file."""
-
-    return bz2_decompress_pickle(app_constants.LAST_SIMULATION_RESULTS)
-
-
-def results_button(results_download_button):
-    if os.path.exists(app_constants.LAST_SIMULATION_RESULTS):
-        last_simultion_timestamp, last_simultion_results = read_simulation_results()
-        results_download_button.download_button(label="Download simulation result from :green["
-                                                + last_simultion_timestamp.strftime("%Y-%m-%d, %H:%M") + " UTC]",
-                                                help="Download simulation result from last run that was finished at "
-                                                + last_simultion_timestamp.strftime("%Y-%m-%d, %H:%M") + " UTC",
-                                                data=pickle.dumps(last_simultion_results),
-                                                file_name="simulation_results_"
-                                                + last_simultion_timestamp.strftime("%Y%m%d%H%M") + ".pickle",
-                                                mime='application/octet-stream')
-    else:
-        results_download_button.download_button(label="Download simulation results", data=b'placeholder',
-                                                disabled=True)
-# ----------------------------------- End save result functions -------------------------------
 
 
 def set_max_width(width: str):
@@ -71,11 +36,6 @@ def download_df_as_csv_button(df: pd.DataFrame, file_name: str, include_index: b
     st.download_button(label='Download as csv',
                        data=csv,
                        file_name=file_name + ".csv")
-    
-
-# @st.cache_data()
-def load_results(uploaded_results_file):
-    st.session_state.simulation_results = pickle.load(uploaded_results_file)
 
 
 def update_multiselect_style():
