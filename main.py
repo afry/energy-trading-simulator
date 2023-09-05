bdbd4fae8ef51d677b30f062e544ca9e994e2bbc
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2023-09-05 13:24:22 +0200

    CLEANUP - changed TradingSimulator.__call__ to not return anything, and removed pickle file saving from main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import pickle
 from logging.handlers import TimedRotatingFileHandler
 
 from pkg_resources import resource_filename
@@ -63,13 +62,7 @@ if __name__ == '__main__':
     create_db_and_tables()
     insert_default_config_into_db()
     with SessionMaker() as sess:
-        job_id = get_job_id_for_config(DEFAULT_CONFIG_NAME, sess)
+        job_id = get_job_id_for_config(args.config_id, sess)
     delete_job(job_id)
     simulator = TradingSimulator(args.config_id)
-    simulation_results = simulator()
-    if simulation_results is not None:
-        logger.info("Finished running simulations. Will save simulations results as a pickle file.")
-        with open(results_path + 'simulation_results.pickle', 'wb') as destination:
-            pickle.dump(simulation_results, destination)
-    else:
-        logger.info("Could not finish simulation.")
+    simulator()
