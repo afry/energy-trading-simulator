d37af94876696992dfb11152414a4ef2f3170963
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-09-06 09:23:04 +0200

    Check that nordpool data contains at least the same range of periods as the input data plus max possible n hours back for BatteryAgent

--- tradingplatformpoc/sql/input_electricity_price/crud.py
+++ tradingplatformpoc/sql/input_electricity_price/crud.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from contextlib import _GeneratorContextManager
 from typing import Callable
@@ -8,6 +9,7 @@ from sqlalchemy import func
 
 from sqlmodel import Session
 
+from tradingplatformpoc.config.access_config import read_parameter_from_agent_defaults
 from tradingplatformpoc.connection import session_scope
 from tradingplatformpoc.data.preproccessing import clean, read_nordpool_data
 from tradingplatformpoc.sql.input_data.models import InputData
@@ -32,13 +34,14 @@ def insert_input_electricity_price_to_db_if_empty(session_generator: Callable[[]
             # Check that the nordpool data contains enough periods
             period_range = db.query(func.max(InputData.period).label('max'),
                                     func.min(InputData.period).label('min')).first()
+            n_hours_back_max = read_parameter_from_agent_defaults('BatteryAgent', 'NHoursBack')['max_value']
             if (electricity_price_df.period.max() >= period_range.max) \
-               & (electricity_price_df.period.min() <= period_range.min):
+               & (electricity_price_df.period.min() <= (period_range.min - datetime.timedelta(hours=n_hours_back_max))):
 
                 electricity_price_dict = electricity_price_df.to_dict(orient='records')
                 db.bulk_insert_mappings(InputElectricityPrice, electricity_price_dict)
             else:
-                logger.error('Nordpool data contains less periods than input data.')
-                raise Exception('Nordpool data contains less periods than input data.')
+                logger.error('Nordpool data contains less periods than input data plus max of n hours back.')
+                raise Exception('Nordpool data contains less periods than input data plus max of n hours back.')
         else:
             logger.info('Input data table already populated.')
