0dad7ca348116ad97a439f4bcccfa77e8194c6c6
Ingrid Ingemarsson <ingrid.ingemarsson@afry.com>
2023-09-14 16:49:08 +0200

    Renaming of function to mock_data_df_to_db_dict

--- tradingplatformpoc/sql/mock_data/crud.py
+++ tradingplatformpoc/sql/mock_data/crud.py
@@ -16,8 +16,8 @@ from tradingplatformpoc.sql.mock_data.models import MockData
 logger = logging.getLogger(__name__)
 
 
-def db_to_mock_data_dict(db_agent_id: str, mock_data_constants: Dict[str, float],
-                         agent_mock_data_pl: pl.DataFrame):
+def mock_data_df_to_db_dict(db_agent_id: str, mock_data_constants: Dict[str, float],
+                            agent_mock_data_pl: pl.DataFrame):
     """
     Convert mock data from dataframe to binary in order to store in database.
     """
