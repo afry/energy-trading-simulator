6e0734bd6681df1faaa1490b8177de8e6adc64a3
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-01-29 15:53:01 +0100

    Added function for getting for example heat pump production from the optimized model

--- tests/test_optimization_problem.py
+++ tests/test_optimization_problem.py
@@ -5,7 +5,8 @@ from unittest import TestCase
 import pyomo.environ as pyo
 
 from tradingplatformpoc.settings import settings
-from tradingplatformpoc.simulation_runner.optimization_problem import get_power_transfers, mock_opt_problem
+from tradingplatformpoc.simulation_runner.optimization_problem import add_value_per_agent_to_dict, \
+    get_power_transfers, mock_opt_problem
 
 
 class Test(TestCase):
@@ -15,7 +16,16 @@ class Test(TestCase):
     else:
         solver = pyo.SolverFactory('glpk', executable=settings.GLPK_PATH)
 
-    def test_mock_opt_problem(self):
+    def test_get_power_transfers(self):
         mod, res = mock_opt_problem(self.solver)
         transfers = get_power_transfers(mod, datetime.datetime(2024, 2, 1))
-        print(len(transfers))
+        expected_length = len(mod.time) * (len(mod.agent) + 1)  # One for each agent, plus one for external
+        self.assertEqual(expected_length, len(transfers))
+
+    def test_get_heat_pump_production(self):
+        mod, res = mock_opt_problem(self.solver)
+        my_dict = {}
+        add_value_per_agent_to_dict(mod, datetime.datetime(2024, 2, 1), my_dict, 'Hhp')
+        self.assertEqual(len(mod.agent), len(my_dict))
+        self.assertEqual(len(mod.time), len(list(my_dict.values())[0]))
+        self.assertEqual(0, list(list(my_dict.values())[0].values())[0])
--- tradingplatformpoc/simulation_runner/optimization_problem.py
+++ tradingplatformpoc/simulation_runner/optimization_problem.py
@@ -1,6 +1,6 @@
 import datetime
 import logging
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 
@@ -11,6 +11,7 @@ from pyomo.opt import SolverResults, TerminationCondition
 
 from tradingplatformpoc.market.bid import Action, Resource
 from tradingplatformpoc.market.trade import Market, Trade
+from tradingplatformpoc.trading_platform_utils import add_to_nested_dict
 
 logger = logging.getLogger(__name__)
 
@@ -26,14 +27,7 @@ def mock_opt_problem(solver) -> Tuple[pyo.ConcreteModel, SolverResults]:
     n_agents = 4
     model.time = pyo.Set(initialize=range(hours_to_run))  # index of time intervals
     model.agent = pyo.Set(initialize=range(n_agents))  # index of agents
-    # Define variables
-    model.Pbuy_market = pyo.Var(model.time, within=pyo.NonNegativeReals, initialize=0)
-    model.Psell_market = pyo.Var(model.time, within=pyo.NonNegativeReals, initialize=0)
-    model.U_buy_sell_market = pyo.Var(model.time, within=pyo.Binary, initialize=0)
-    model.Pbuy_grid = pyo.Var(model.agent, model.time, within=pyo.NonNegativeReals, initialize=0)
-    model.Psell_grid = pyo.Var(model.agent, model.time, within=pyo.NonNegativeReals, initialize=0)
-    model.U_power_buy_sell_grid = pyo.Var(model.agent, model.time, within=pyo.Binary, initialize=0)
-
+    # Parameters
     model.price_buy = pyo.Param(model.time, initialize=np.random.randn(hours_to_run) + 1)
     model.price_sell = pyo.Param(model.time, initialize=0.9 * (np.ones(hours_to_run) + 1))
     model.power_max_grid = pyo.Param(initialize=500)
@@ -42,6 +36,16 @@ def mock_opt_problem(solver) -> Tuple[pyo.ConcreteModel, SolverResults]:
     power_pv_df = pd.DataFrame(np.random.randn(n_agents, hours_to_run))
     model.power_dem = pyo.Param(model.agent, model.time, initialize=lambda m, i, t: power_dem_df.iloc[i, t])
     model.power_pv = pyo.Param(model.agent, model.time, initialize=lambda m, i, t: power_pv_df.iloc[i, t])
+    model.Hhpmax = pyo.Param(initialize=70)
+    # Define variables
+    model.Pbuy_market = pyo.Var(model.time, within=pyo.NonNegativeReals, initialize=0)
+    model.Psell_market = pyo.Var(model.time, within=pyo.NonNegativeReals, initialize=0)
+    model.U_buy_sell_market = pyo.Var(model.time, within=pyo.Binary, initialize=0)
+    model.Pbuy_grid = pyo.Var(model.agent, model.time, within=pyo.NonNegativeReals, initialize=0)
+    model.Psell_grid = pyo.Var(model.agent, model.time, within=pyo.NonNegativeReals, initialize=0)
+    model.U_power_buy_sell_grid = pyo.Var(model.agent, model.time, within=pyo.Binary, initialize=0)
+    model.Hhp = pyo.Var(model.agent, model.time, bounds=(0, model.Hhpmax), within=pyo.NonNegativeReals, initialize=0)
+    model.SOCBES = pyo.Var(model.agent, model.time, bounds=(0, 1), within=pyo.NonNegativeReals, initialize=0)
 
     # Define objective function
     model.obj = pyo.Objective(rule=lambda m, t: sum(m.Pbuy_market[t] * m.price_buy[t]
@@ -82,6 +86,7 @@ def mock_opt_problem(solver) -> Tuple[pyo.ConcreteModel, SolverResults]:
 
 
 def get_power_transfers(optimized_model: pyo.ConcreteModel, start_datetime: datetime.datetime) -> List[Trade]:
+    # For example: Pbuy_market is how much the LEC bought from the external grid operator
     return get_transfers(optimized_model, start_datetime,
                          sold_to_external_name='Psell_market', bought_from_external_name='Pbuy_market',
                          sold_internal_name='Psell_grid', bought_internal_name='Pbuy_grid')
@@ -95,26 +100,54 @@ def get_transfers(optimized_model: pyo.ConcreteModel, start_datetime: datetime.d
     """
     transfers = []
     for hour in optimized_model.time:
-        # Pbuy_market is how much the LEC bought from the external grid operator
-        external_quantity = pyo.value(getattr(optimized_model, sold_to_external_name)[hour]
-                                      - getattr(optimized_model, bought_from_external_name)[hour])
-        transfers.append(Trade(period=start_datetime + datetime.timedelta(hours=hour),
-                               action=Action.BUY if external_quantity > 0 else Action.SELL,
-                               resource=Resource.ELECTRICITY,
-                               quantity=abs(external_quantity),
-                               price=np.nan,
-                               source='ExternalGridAgent',
-                               by_external=False,
-                               market=Market.LOCAL))
+        # TODO: grid_agent_guid
+        trade = construct_external_trade(bought_from_external_name, hour, optimized_model, sold_to_external_name,
+                                         start_datetime, 'ExternalGridAgent', Resource.ELECTRICITY)
+        transfers.append(trade)
         for i_agent in optimized_model.agent:
-            quantity = pyo.value(getattr(optimized_model, bought_internal_name)[i_agent, hour]
-                                 - getattr(optimized_model, sold_internal_name)[i_agent, hour])
-            transfers.append(Trade(period=start_datetime + datetime.timedelta(hours=hour),
-                                   action=Action.BUY if quantity > 0 else Action.SELL,
-                                   resource=Resource.ELECTRICITY,
-                                   quantity=abs(quantity),
-                                   price=np.nan,
-                                   source=str(i_agent),
-                                   by_external=False,
-                                   market=Market.LOCAL))
+            t = construct_agent_trade(bought_internal_name, sold_internal_name, hour, i_agent, optimized_model,
+                                      start_datetime, Resource.ELECTRICITY)
+            transfers.append(t)
     return transfers
+
+
+def construct_agent_trade(bought_internal_name: str, sold_internal_name: str, hour: int, i_agent: int,
+                          optimized_model: pyo.ConcreteModel, start_datetime: datetime.datetime, resource: Resource) \
+        -> Trade:
+    quantity = pyo.value(getattr(optimized_model, bought_internal_name)[i_agent, hour]
+                         - getattr(optimized_model, sold_internal_name)[i_agent, hour])
+    agent_name = agent_guid_from_index(optimized_model, i_agent)
+    return Trade(period=start_datetime + datetime.timedelta(hours=hour),
+                 action=Action.BUY if quantity > 0 else Action.SELL, resource=resource,
+                 quantity=abs(quantity), price=np.nan, source=agent_name, by_external=False, market=Market.LOCAL)
+
+
+def construct_external_trade(bought_from_external_name: str, hour: int, optimized_model: pyo.ConcreteModel,
+                             sold_to_external_name: str, start_datetime: datetime.datetime, grid_agent_guid: str,
+                             resource: Resource) -> Trade:
+    external_quantity = pyo.value(getattr(optimized_model, sold_to_external_name)[hour]
+                                  - getattr(optimized_model, bought_from_external_name)[hour])
+    return Trade(period=start_datetime + datetime.timedelta(hours=hour),
+                 action=Action.BUY if external_quantity > 0 else Action.SELL, resource=resource,
+                 quantity=abs(external_quantity), price=np.nan, source=grid_agent_guid, by_external=True,
+                 market=Market.LOCAL)
+
+
+def agent_guid_from_index(optimized_model: pyo.ConcreteModel, i_agent: int) -> str:
+    # TODO: Translate index to agent GUID in some way.
+    #  Perhaps use names from the input data frames on the model object, or pass it separately, into an intermediate
+    #  method, which lives between trading_simulator and the Chalmers code, and then use here
+    return str(i_agent)
+
+
+def add_value_per_agent_to_dict(optimized_model: pyo.ConcreteModel, start_datetime: datetime.datetime,
+                                dict_to_add_to: Dict[str, Dict[datetime.datetime, Any]],
+                                variable_name: str):
+    """
+    Example variable names: Hhp for heat pump production, SOCBES for state of charge of battery storage
+    """
+    for hour in optimized_model.time:
+        for i_agent in optimized_model.agent:
+            quantity = pyo.value(getattr(optimized_model, variable_name)[i_agent, hour])
+            period = start_datetime + datetime.timedelta(hours=hour)
+            add_to_nested_dict(dict_to_add_to, agent_guid_from_index(optimized_model, i_agent), period, quantity)
