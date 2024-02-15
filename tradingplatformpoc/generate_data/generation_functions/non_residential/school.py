456fe84155e98e64f8a688d63b9f79861ea56898
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-02-15 11:24:11 +0100

    Small changes to school mock data generation

--- tradingplatformpoc/generate_data/generation_functions/non_residential/school.py
+++ tradingplatformpoc/generate_data/generation_functions/non_residential/school.py
@@ -7,6 +7,10 @@ from tradingplatformpoc.generate_data.generation_functions.non_residential.commo
     simulate_space_heating
 
 
+def get_monday_of_week(year: int, week_number: int) -> datetime.datetime:
+    return datetime.datetime.strptime(str(year) + '-W' + str(week_number) + '-1', "%Y-W%W-%w")
+
+
 # Constants used in the 'is_break' method. Better to instantiate these here, since is_break is called many times, and
 # instantiating these over and over again is really unnecessary.
 # Year doesn't really matter, we'll only use the day-of-year
@@ -14,20 +18,20 @@ JUST_SOME_NONE_LEAP_YEAR = 2019
 # Summer break 15/6 - 15/8
 SUMMER_START = datetime.datetime(JUST_SOME_NONE_LEAP_YEAR, 6, 15).timetuple().tm_yday
 SUMMER_END = SUMMER_START + 60
-# Fall break 1/11 - 7/11
-FALL_START = datetime.datetime(JUST_SOME_NONE_LEAP_YEAR, 11, 1).timetuple().tm_yday
+# Fall break week 44
+FALL_START = get_monday_of_week(JUST_SOME_NONE_LEAP_YEAR, 44).timetuple().tm_yday
 FALL_END = FALL_START + 7
 # Christmas break 22/12 - 2/1
 CHRISTMAS_START = datetime.datetime(JUST_SOME_NONE_LEAP_YEAR, 12, 22).timetuple().tm_yday
 CHRISTMAS_END = CHRISTMAS_START + 14
-# Sportlov 15/2 - 21/2
-SPRING_START = datetime.datetime(JUST_SOME_NONE_LEAP_YEAR, 2, 1).timetuple().tm_yday
+# Sportlov week 8
+SPRING_START = get_monday_of_week(JUST_SOME_NONE_LEAP_YEAR, 8).timetuple().tm_yday
 SPRING_END = SPRING_START + 7
-# Easter 07/04 - 14/04
+# Easter week 15
 # Easter moves yearly, but since we are only interested in capturing the feature
-# of a week off school sometime in mid-spring, we simply chose an average date (April 7th)
-EASTER_START = datetime.datetime(JUST_SOME_NONE_LEAP_YEAR, 4, 7).timetuple().tm_yday
-EASTER_END = EASTER_START + 7
+# of a week off school sometime in mid-spring, we simply chose an average date (early-mid April)
+EASTER_START = get_monday_of_week(JUST_SOME_NONE_LEAP_YEAR, 15).timetuple().tm_yday
+EASTER_END = EASTER_START + 8
 
 
 def is_break(timestamp: datetime.datetime):
