fe68b25d8bfaf601e465929a4565c5e0f3a98a53
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-02-20 09:35:31 +0100

    Reordered columns of leaderboard slightly

--- tradingplatformpoc/app/pages/results_summary.py
+++ tradingplatformpoc/app/pages/results_summary.py
@@ -7,6 +7,7 @@ from st_pages import add_indentation, show_pages_from_config
 import streamlit as st
 
 from tradingplatformpoc.sql.results.crud import get_all_results
+from tradingplatformpoc.sql.results.models import ResultsKey
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,12 @@ add_indentation()
 list_of_dicts = get_all_results()
 if len(list_of_dicts):
     df_to_display = pd.DataFrame.from_records(list_of_dicts, index='Config ID')
+
+    # Reorder columns slightly
+    first_columns = [ResultsKey.SUM_LEC_EXPENDITURE, ResultsKey.SUM_NET_IMPORT_HEAT, ResultsKey.SUM_NET_IMPORT_ELEC]
+    columns_order = first_columns + [col for col in df_to_display.columns if col not in first_columns]
+    df_to_display = df_to_display[columns_order]
+
     st.dataframe(df_to_display.round(decimals=0))
 else:
     st.markdown('No results to display. Set up a configuration in '
