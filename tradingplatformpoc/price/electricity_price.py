34c9bb718914df8c8ae34e653dc15465a299288f
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-02-26 14:13:50 +0100

    RES-413 - methods for getting electricity price in batches, as pd.Series, which will be useful when implementing the Chalmers solver

--- tradingplatformpoc/price/electricity_price.py
+++ tradingplatformpoc/price/electricity_price.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+from typing import Union
 
 import pandas as pd
 
@@ -54,66 +55,88 @@ class ElectricityPrice(IPrice):
 
     def get_exact_retail_price(self, period: datetime.datetime, include_tax: bool) -> float:
         """Returns the price at which the external grid operator is willing to sell energy, in SEK/kWh"""
-        gross_price = self.get_electricity_gross_retail_price(period)
+        return self.get_exact_retail_prices(period, 1, include_tax)
+
+    def get_exact_wholesale_price(self, period: datetime.datetime) -> float:
+        """Returns the price at which the external grid operator is willing to buy energy, in SEK/kWh"""
+        return self.get_exact_wholesale_prices(period, 1)
+
+    def get_exact_retail_prices(self, period: datetime.datetime, length: int, include_tax: bool) \
+            -> Union[float, pd.Series]:
+        """
+        Returns the price at which the external grid operator is willing to sell energy, in SEK/kWh, for the
+        start_period and length periods forwards.
+        """
+        gross_price = self.get_electricity_gross_retail_price(period, length)
         if include_tax:
             return self.get_electricity_net_external_price(gross_price)
         else:
             return gross_price
 
-    def get_exact_wholesale_price(self, period: datetime.datetime) -> float:
-        """Returns the price at which the external grid operator is willing to buy energy, in SEK/kWh"""
+    def get_exact_wholesale_prices(self, period: datetime.datetime, length: int) -> Union[float, pd.Series]:
+        """
+        Returns the price at which the external grid operator is willing to buy energy, in SEK/kWh, for the
+        start_period and length periods forwards.
+        """
+        return self.get_electricity_wholesale_price(period, length)
 
-        return self.get_electricity_wholesale_price(period)
-
-    def get_electricity_gross_retail_price(self, period: datetime.datetime) -> float:
+    def get_electricity_gross_retail_price(self, period: datetime.datetime, length: int = 1) -> Union[float, pd.Series]:
         """
         For electricity, the price is known, so 'estimated' and 'exact' are the same.
         See also https://doc.afdrift.se/pages/viewpage.action?pageId=17072325
         """
-        nordpool_price = self.get_nordpool_price_for_period(period)
+        nordpool_price = self.get_nordpool_price_for_periods(period, length)
         return self.get_electricity_gross_retail_price_from_nordpool_price(nordpool_price)
     
-    def get_electricity_gross_retail_price_from_nordpool_price(self, nordpool_price: float) -> float:
+    def get_electricity_gross_retail_price_from_nordpool_price(self, nordpool_price: Union[float, pd.Series]) \
+            -> Union[float, pd.Series]:
         """
         The external grid sells at the Nordpool spot price, plus the "grid fee".
         See also https://doc.afdrift.se/pages/viewpage.action?pageId=17072325
         """
         return nordpool_price + self.elec_grid_fee
 
-    def get_electricity_net_external_price(self, gross_price: float) -> float:
+    def get_electricity_net_external_price(self, gross_price: Union[float, pd.Series]) -> Union[float, pd.Series]:
         """
         Net external price = gross external price (i.e. what the seller receives) + tax
         """
         return gross_price + self.elec_tax
 
-    def get_electricity_net_internal_price(self, gross_price: float) -> float:
+    def get_electricity_net_internal_price(self, gross_price: Union[float, pd.Series]) -> Union[float, pd.Series]:
         """
         Net internal price = gross price (i.e. what the seller receives) + tax + grid fee
         """
         return gross_price + self.elec_tax_internal + self.elec_grid_fee_internal
 
-    def get_electricity_gross_internal_price(self, net_price: float) -> float:
+    def get_electricity_gross_internal_price(self, net_price: Union[float, pd.Series]) -> Union[float, pd.Series]:
         """
         Given a "net" price, for example the market clearing price, this method calculates how much a seller actually
         receives after paying taxes and grid fees.
         """
         return net_price - self.elec_tax_internal - self.elec_grid_fee_internal
 
-    def get_electricity_wholesale_price(self, period: datetime.datetime) -> float:
+    def get_electricity_wholesale_price(self, period: datetime.datetime, length: int = 1) -> Union[float, pd.Series]:
         """
         For electricity, the price is known, so 'estimated' and 'exact' are the same.
         See also https://doc.afdrift.se/pages/viewpage.action?pageId=17072325
         """
-        return self.get_electricity_wholesale_price_from_nordpool_price(self.get_nordpool_price_for_period(period))
+        nordpool_prices = self.get_nordpool_price_for_periods(period, length)
+        return self.get_electricity_wholesale_price_from_nordpool_price(nordpool_prices)
 
-    def get_electricity_wholesale_price_from_nordpool_price(self, nordpool_price: float) -> float:
+    def get_electricity_wholesale_price_from_nordpool_price(self, nordpool_price: Union[float, pd.Series]) \
+            -> Union[float, pd.Series]:
         """
         Wholesale price = Nordpool spot price + self.elec_wholesale_offset
         """
         return nordpool_price + self.elec_wholesale_offset
-    
-    def get_nordpool_price_for_period(self, period: datetime.datetime):
-        return self.nordpool_data.loc[period]
+
+    def get_nordpool_price_for_periods(self, start_period: datetime.datetime, length: int = 1) \
+            -> Union[float, pd.Series]:
+        if length == 1:
+            return self.nordpool_data.loc[start_period]
+        start_index = self.nordpool_data.index.get_loc(start_period)
+        end_index = start_index + length
+        return self.nordpool_data.iloc[start_index:end_index]
     
     def get_nordpool_prices_last_n_hours_dict(self, period: datetime.datetime, go_back_n_hours: int):
         mask = (self.nordpool_data.index < period) & \
