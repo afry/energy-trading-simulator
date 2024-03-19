8366c55904a3ff9ff387d1e051aa5b0a775cdcfe
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-03-19 14:38:35 +0100

    Updated version number for development

--- tradingplatformpoc/simulation_runner/trading_simulator.py
+++ tradingplatformpoc/simulation_runner/trading_simulator.py
@@ -79,9 +79,9 @@ class TradingSimulator:
 
         self.trading_periods = get_periods_from_db().sort_values()
         # FIXME: Remove
-        # self.trading_periods = self.trading_periods.take(list(range(4008, 4032))
-        #                                                  + list(range(5520, 5544))
-        #                                                  + list(range(5952, 5976)))
+        self.trading_periods = self.trading_periods.take(list(range(4008, 4032))
+                                                         + list(range(5520, 5544))
+                                                         + list(range(5952, 5976)))
         self.trading_horizon = self.config_data['AreaInfo']['TradingHorizon']
 
     def initialize_agents(self) -> Tuple[List[IAgent], Dict[Resource, GridAgent]]:
