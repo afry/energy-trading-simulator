0bb965feba9d3d0fe8a79c810be9f41075021343
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-04-08 10:42:00 +0200

    Updated version number for development

--- tradingplatformpoc/app/footer.py
+++ tradingplatformpoc/app/footer.py
@@ -37,7 +37,7 @@ text-align: left; /* you can replace 'left' with 'center' or 'right' if you want
 <div id="page-container">
 
 <div class="footer">
-Version 3.6.0<br>
+Version 3.6.1.dev<br>
 Built by AFRY AI & Analytics
 </div>
 
