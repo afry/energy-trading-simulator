8511fb0e758632124056a3dec90171e1b9195313
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-04-09 09:49:16 +0200

    Added test and refactored config_data_feasibility_screening slightly

--- tests/test_app_functions.py
+++ tests/test_app_functions.py
@@ -1,11 +1,30 @@
 from unittest import TestCase
 
 from tradingplatformpoc.app.app_functions import config_naming_is_valid, has_control_characters
-from tradingplatformpoc.config.screen_config import config_data_agent_screening, config_data_keys_screening, \
-    config_data_param_screening
+from tradingplatformpoc.config.screen_config import config_data_agent_screening, config_data_feasibility_screening, \
+    config_data_keys_screening, config_data_param_screening
 
 
 class TestAppFunctions(TestCase):
+    mock_grid_el = {"Type": "GridAgent", "Name": "ElectricityGridAgent",
+                    "Resource": "ELECTRICITY", "TransferRate": 5.0}
+    mock_grid_he = {"Type": "GridAgent", "Name": "HeatingGridAgent",
+                    "Resource": "HIGH_TEMP_HEAT", "TransferRate": 5.0}
+    mock_pv = {"Type": "BlockAgent",
+               "Name": "PVParkAgent",
+               "Atemp": 0.0,
+               "FractionCommercial": 0.0,
+               "FractionSchool": 0.0,
+               "FractionOffice": 0.0,
+               "HeatPumpMaxInput": 0.0,
+               "HeatPumpMaxOutput": 0.0,
+               "BoosterPumpMaxInput": 0.0,
+               "BoosterPumpMaxOutput": 0.0,
+               "BatteryCapacity": 0.0,
+               "AccumulatorTankCapacity": 0.0,
+               "FractionUsedForBITES": 0.0,
+               "HeatPumpForCooling": False,
+               "PVArea": 20.0}
 
     def test_config_data_json_screening(self):
         """Test of check for unknown keys."""
@@ -30,50 +49,31 @@ class TestAppFunctions(TestCase):
 
     def test_config_data_agent_screening(self):
         """Test of agent input check."""
-        mock_grid_el = {"Type": "GridAgent", "Name": "ElectricityGridAgent",
-                        "Resource": "ELECTRICITY", "TransferRate": 5.0}
-        mock_grid_he = {"Type": "GridAgent", "Name": "HeatingGridAgent",
-                        "Resource": "HIGH_TEMP_HEAT", "TransferRate": 5.0}
-        mock_pv = {"Type": "BlockAgent",
-                   "Name": "PVParkAgent",
-                   "Atemp": 0.0,
-                   "FractionCommercial": 0.0,
-                   "FractionSchool": 0.0,
-                   "FractionOffice": 0.0,
-                   "HeatPumpMaxInput": 0.0,
-                   "HeatPumpMaxOutput": 0.0,
-                   "BoosterPumpMaxInput": 0.0,
-                   "BoosterPumpMaxOutput": 0.0,
-                   "BatteryCapacity": 0.0,
-                   "AccumulatorTankCapacity": 0.0,
-                   "FractionUsedForBITES": 0.0,
-                   "HeatPumpForCooling": False,
-                   "PVArea": 20.0}
 
-        self.assertIsNone(config_data_agent_screening({'Agents': [mock_grid_el, mock_grid_he, mock_pv]}))
+        self.assertIsNone(config_data_agent_screening({'Agents': [self.mock_grid_el, self.mock_grid_he, self.mock_pv]}))
 
         # No Type
         self.assertEqual('Agent ElectricityGridAgent provided without \'Type\'.',
                          config_data_agent_screening({'Agents': [{"Name": "ElectricityGridAgent",
                                                                   "Resource": "ELECTRICITY",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
         # No Name
         self.assertEqual('Agent of type GridAgent provided without \'Name\'.',
                          config_data_agent_screening({'Agents': [{"Type": "GridAgent",
                                                                   "Resource": "ELECTRICITY",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
         
         # Missing resource for GridAgent
         self.assertEqual("No specified resource for agent ElectricityGridAgent.",
                          config_data_agent_screening({'Agents': [{"Type": "GridAgent",
                                                                   "Name": "ElectricityGridAgent",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
         
         # Too many GridAgents
         self.assertEqual('Too many GridAgents provided, should be one for each resource!',
@@ -81,9 +81,9 @@ class TestAppFunctions(TestCase):
                                                                   "Name": "ElectricityGridAgent",
                                                                   'Resource': "ELECTRICITY",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_el,
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_el,
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
         
         # Invalid resource
         self.assertEqual("Resource ThisIsNotAValidResource is not in available for agent ElectricityGridAgent.",
@@ -91,28 +91,29 @@ class TestAppFunctions(TestCase):
                                                                   "Name": "ElectricityGridAgent",
                                                                   "Resource": "ThisIsNotAValidResource",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
 
         # Missing GridAgents
         self.assertEqual('No GridAgent provided!',
-                         config_data_agent_screening({'Agents': [mock_pv]}))
+                         config_data_agent_screening({'Agents': [self.mock_pv]}))
 
         # Missing GridAgent HIGH_TEMP_HEAT
         self.assertEqual('No GridAgent with resource: HIGH_TEMP_HEAT provided!',
-                         config_data_agent_screening({'Agents': [mock_pv, mock_grid_el]}))
+                         config_data_agent_screening({'Agents': [self.mock_pv, self.mock_grid_el]}))
 
         # Missing GridAgent ELECTRICITY
         self.assertEqual('No GridAgent with resource: ELECTRICITY provided!',
-                         config_data_agent_screening({'Agents': [mock_pv, mock_grid_he]}))
+                         config_data_agent_screening({'Agents': [self.mock_pv, self.mock_grid_he]}))
 
         # Missing one agent that is not a GridAgent
         self.assertEqual('No non-GridAgents provided, needs at least one other agent!',
-                         config_data_agent_screening({'Agents': [mock_grid_el, mock_grid_he]}))
+                         config_data_agent_screening({'Agents': [self.mock_grid_el, self.mock_grid_he]}))
         
         # Too many GridAgents
         self.assertEqual('Too many GridAgents provided, should be one for each resource!',
-                         config_data_agent_screening({'Agents': [mock_grid_el, mock_grid_he, mock_grid_he]}))
+                         config_data_agent_screening(
+                             {'Agents': [self.mock_grid_el, self.mock_grid_he, self.mock_grid_he]}))
                              
         # Invalid agent Type
         self.assertEqual('Agent ThisIsNotAValidAgent provided with unrecognized \'Type\' ThisIsNotAValidAgentType.',
@@ -120,28 +121,28 @@ class TestAppFunctions(TestCase):
                                                                   "Name": "ThisIsNotAValidAgent",
                                                                   "Resource": "ELECTRICITY",
                                                                   "TransferRate": 5.0},
-                                                                 mock_grid_he,
-                                                                 mock_grid_el]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_grid_el]}))
         
         # Missing required parameter
         self.assertEqual("Missing parameter TransferRate for agent ElectricityGridAgent.",
                          config_data_agent_screening({'Agents': [{"Type": "GridAgent",
                                                                   "Name": "ElectricityGridAgent",
                                                                   "Resource": "ELECTRICITY"},
-                                                                 mock_grid_he,
-                                                                 mock_pv]}))
+                                                                 self.mock_grid_he,
+                                                                 self.mock_pv]}))
         
         # Unknown parameter
         self.assertEqual("Specified ThisIsNotAValidAgentParameter not in available "
                          "input params for agent HeatingGridAgent of type GridAgent.",
-                         config_data_agent_screening({'Agents': [{**mock_grid_he,
+                         config_data_agent_screening({'Agents': [{**self.mock_grid_he,
                                                                   **{'ThisIsNotAValidAgentParameter': 0.0}},
-                                                                 mock_grid_el, mock_pv]}))
+                                                                 self.mock_grid_el, self.mock_pv]}))
 
         # TransferRate of Storage Agent min value: 0.0
-        mock_grid_he['TransferRate'] = -0.1
+        self.mock_grid_he['TransferRate'] = -0.1
         self.assertEqual("Specified TransferRate: -0.1 < 0.0.",
-                         config_data_agent_screening({'Agents': [mock_grid_he, mock_grid_el, mock_pv]}))
+                         config_data_agent_screening({'Agents': [self.mock_grid_he, self.mock_grid_el, self.mock_pv]}))
 
     def test_config_naming_is_valid(self):
         """Test that we allow/disallow config names appropriately"""
@@ -152,3 +153,33 @@ class TestAppFunctions(TestCase):
     def test_has_control_characters(self):
         self.assertTrue(has_control_characters('abc\x0123'))
         self.assertFalse(has_control_characters('Just a normal string'))
+
+    def test_screen_feasibility(self):
+        config_data = {'Agents': [self.mock_grid_el, self.mock_grid_he, self.mock_pv],
+                       'AreaInfo': {'CompChillerMaxInput': 0.0,
+                                    'CompChillerCOP': 0.0,
+                                    'COPHeatPumpsHighTemp': 3.0,
+                                    'COPHeatPumpsLowTemp': 2.0}}
+        self.assertIsNone(config_data_feasibility_screening(config_data))
+
+        config_data = {'Agents': [self.mock_grid_el, self.mock_grid_he, self.mock_pv,
+                                  {"Type": "BlockAgent",
+                                   "Name": "AgentWithoutBoosterHP",
+                                   "Atemp": 1000.0,
+                                   "FractionCommercial": 0.0,
+                                   "FractionSchool": 0.0,
+                                   "FractionOffice": 0.0,
+                                   "HeatPumpMaxInput": 0.0,
+                                   "HeatPumpMaxOutput": 0.0,
+                                   "BoosterPumpMaxInput": 0.0,
+                                   "BoosterPumpMaxOutput": 0.0,
+                                   "BatteryCapacity": 0.0,
+                                   "AccumulatorTankCapacity": 0.0,
+                                   "FractionUsedForBITES": 0.0,
+                                   "HeatPumpForCooling": False,
+                                   "PVArea": 0.0}],
+                       'AreaInfo': {'CompChillerMaxInput': 0.0,
+                                    'CompChillerCOP': 0.0,
+                                    'COPHeatPumpsHighTemp': 3.0,
+                                    'COPHeatPumpsLowTemp': 2.0}}
+        self.assertIsNotNone(config_data_feasibility_screening(config_data))
