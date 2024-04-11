fccc03a221e6188f8606b718758b9509177d0740
Gustav Ljungqvist <gustav.ljungqvist@afry.com>
2024-04-11 16:32:53 +0200

    Chalmers update - removed cooling dump penalty

--- tradingplatformpoc/simulation_runner/CEMS_function.py
+++ tradingplatformpoc/simulation_runner/CEMS_function.py
@@ -93,10 +93,10 @@ def solve_model(solver: OptSolver, summer_mode: bool, month: int, n_agents: int,
 
     # Similarly, check the maximum cooling produced vs the cooling demand
     max_cooling_produced_for_1_hour = Pccmax * chiller_COP \
-                                      + sum([(hp_cop - 1) * max_php if hpc_active else
-                                             np.inf if has_bh and month not in [6, 7, 8] else 0
-                                             for hp_cop, max_php, hpc_active, has_bh in
-                                             zip(heatpump_COP, heatpump_max_power, HP_Cproduct_active, borehole)])
+        + sum([(hp_cop - 1) * max_php if hpc_active else
+               np.inf if has_bh and month not in [6, 7, 8] else 0
+               for hp_cop, max_php, hpc_active, has_bh in
+               zip(heatpump_COP, heatpump_max_power, HP_Cproduct_active, borehole)])
     too_big_cool_demand = cold_consumption.sum(axis=0).gt(max_cooling_produced_for_1_hour)
     if too_big_cool_demand.any():
         problematic_hours = [i for i, x in enumerate(too_big_cool_demand) if x]
@@ -262,8 +262,8 @@ def add_obj_and_constraints(model: pyo.ConcreteModel, summer_mode: bool, month: 
         model.con_chiller_Hwaste_summer = pyo.Constraint(model.T, rule=chiller_Hwaste_summer)
     else:
         model.con_chiller_Hwaste_winter = pyo.Constraint(model.T, rule=chiller_Hwaste_winter)
-    model.con_max_HTES_dis = pyo.Constraint(model.I, model.T, rule=max_HTES_dis)
-    model.con_max_HTES_cha = pyo.Constraint(model.I, model.T, rule=max_HTES_cha)
+#    model.con_max_HTES_dis = pyo.Constraint(model.I, model.T, rule=max_HTES_dis)
+#    model.con_max_HTES_cha = pyo.Constraint(model.I, model.T, rule=max_HTES_cha)
     model.con_HTES_Ebalance = pyo.Constraint(model.I, model.T, rule=HTES_Ebalance)
     model.con_HTES_final_SOC = pyo.Constraint(model.I, rule=HTES_final_SOC)
     model.con_chiller_Cpower_product = pyo.Constraint(model.T, rule=chiller_Cpower_product)
@@ -275,8 +275,7 @@ def obj_rul(model):
     return sum(
         model.Pbuy_market[t] * model.price_buy[t] - model.Psell_market[t] * model.price_sell[t]
         + model.Hbuy_market[t] * model.Hprice_energy
-        + model.cool_dump[t] * model.penalty + sum(model.heat_dump[i, t] for i in model.I) * model.penalty
-        for t in model.T)
+        + sum(model.heat_dump[i, t] for i in model.I) * model.penalty for t in model.T)
 
 
 # Constraints:
@@ -540,12 +539,12 @@ def max_booster_HP_Hproduct_summer(model, i, t):
 
 # Thermal energy storage model (eqs. 32 to 25 of the report)
 # Maximum/minimum temperature limitations of hot water inside TES
-def max_HTES_dis(model, i, t):
-    return model.HTESdis[i, t] <= model.kwh_per_deg[i] * model.Tmax_TES[i]
-
-
-def max_HTES_cha(model, i, t):
-    return model.HTEScha[i, t] <= model.kwh_per_deg[i] * model.Tmax_TES[i]
+# def max_HTES_dis(model, i, t):
+#     return model.HTESdis[i, t] <= model.kwh_per_deg[i] * model.Tmax_TES[i]
+#
+#
+# def max_HTES_cha(model, i, t):
+#     return model.HTEScha[i, t] <= model.kwh_per_deg[i] * model.Tmax_TES[i]
 
 
 # State of charge modelling
